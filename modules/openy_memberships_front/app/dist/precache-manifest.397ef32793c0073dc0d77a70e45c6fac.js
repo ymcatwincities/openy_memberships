cf82da2b634cbbdfb82eef7b3061fee259e422a3
Dmytro Buzinov <dbuzinov@gmail.com>
2020-03-20 15:43:58 +0200

    OS-733: Membership builder app basic infrastructure.

---
+++ modules/openy_memberships_front/app/dist/precache-manifest.397ef32793c0073dc0d77a70e45c6fac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15530f2a1c3dd0aa92b3",
+    "url": "/assets/css/app.css"
+  },
+  {
+    "revision": "c9044a1b5898e7485423",
+    "url": "/assets/css/chunk-vendors.css"
+  },
+  {
+    "revision": "15530f2a1c3dd0aa92b3",
+    "url": "/assets/js/app.js"
+  },
+  {
+    "revision": "c9044a1b5898e7485423",
+    "url": "/assets/js/chunk-vendors.js"
+  },
+  {
+    "revision": "fd8e9bb5ec4454ea3c308cf12f44f6c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb6317ef1763dac47f0956314af32644",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
