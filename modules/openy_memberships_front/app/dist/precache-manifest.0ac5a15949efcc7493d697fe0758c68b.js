a5b42fee380a5a9440a08aa5d26d78e2182f8362
Dmytro Buzinov <dbuzinov@gmail.com>
2020-04-29 19:07:57 +0300

    Udpate build.

---
+++ modules/openy_memberships_front/app/dist/precache-manifest.0ac5a15949efcc7493d697fe0758c68b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "796b253a5fd6bc526ae0",
+    "url": "/assets/css/chunk-vendors.css"
+  },
+  {
+    "revision": "8f581e04296f49e932ff",
+    "url": "/assets/js/app.js"
+  },
+  {
+    "revision": "796b253a5fd6bc526ae0",
+    "url": "/assets/js/chunk-vendors.js"
+  },
+  {
+    "revision": "02c09ca951ff4b31d192d64093ac07cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d91a158721fc15dd1ed4e2dfe58e865",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
