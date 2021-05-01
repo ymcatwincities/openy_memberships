d579d7b000c94af0e88ddbed1af1247296c57834
Andrii Tiupa <andrey@imagexmedia.com>
2021-05-01 08:42:06 +0300

    Memberships styling fixes

--- js/mobile-menu.behaviors.js
+++ js/mobile-menu.behaviors.js
@@ -10,6 +10,9 @@
   Drupal.behaviors.openy_memberships_mobile_menu = {
     attach: function (context) {
       var block_id = '#block-memberships-mobile-menu';
+      if ($('#block-openy-lily-content').length > 0) {
+        block_id = '#block-lily-memberships-mobile-menu';
+      }
       if ($('body').hasClass('rose_based_theme')) {
         block_id = '#block-rose-memberships-mobile-menu';
       }
@@ -18,6 +21,19 @@
         $(block_id + ' h2').toggleClass('menu-open');
         $(block_id + ' .wrapper-field-paragraph').toggleClass('d-block');
       });
+
+      // Make main-region full width for Lily theme (fixes narrow view).
+      if ($('#block-openy-lily-content').length > 0 && $('.ps-block_container-openy-memberships-desktop-sidebar').length > 0) {
+        $('#block-openy-lily-content .main-region')
+          .removeClass('col-sm-8')
+          .addClass('col-md-8');
+      }
+      // Make main-region full width for Rosr theme (fixes narrow view).
+      if ($('#block-openy-rose-content').length > 0 && $('.ps-block_container-openy-memberships-desktop-sidebar').length > 0) {
+        $('#block-openy-rose-content .main-region')
+          .removeClass('col-sm-8')
+          .addClass('col-md-8');
+      }
     }
   };
 
